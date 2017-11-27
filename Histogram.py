78c702c4f9a49ae951aa691ffd56d3110d8cd981
UnPython <33897749+UnPython@users.noreply.github.com>
2017-11-27 18:16:40 +0000

    Add files via upload

---
+++ Histogram.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pylab as plt
+from collections import counter as c
+def histogram():
+    data_histogram = np.recfromcsv("/documents/secure/message")
+#plt.xlabel
+#plt.<histogram>(range(len(data_histogram)), data_histogram["result"], align="center"
+#plt.title("table name")
--- password_finder.py
+++ password_finder.py
@@ -1,14 +1,21 @@
 import os
+
 def findpswrd():
-    for root, dirs, files in os.walk("H:\Year 1 CSM\Principles of Programming\Assignment 1 - Due 4-12-17\POP_Assigment_1-master\documents"): #for name in files
+    x = 0
+    for root, dirs, files in os.walk("documents\documents"): #for name in files
         
         for file_current in files:              #runs a loop for each file in files
             joined_file = os.path.join(root,file_current)
             file_data=open(joined_file, "r")   #opens all the file in the list call files
-            file_contents = file_data.read()    #reads all the data in the text file currently selected
-
-            if ("4D696C6C656E69756D32303030" in file_contents ):           #tests to see if the file was empty
-                print("Found Millenium2000 in {}".format(file_current))
-            #print(file_contents)
-
+            file_contents = file_data.read()    #reads all the data in the textfile currently selected
+            decode = file_contents.decode("hex")
+            x +=1
+            
+            if ("Millenium2000" in decode ):#this should
+                print("Found Millenium2000 in {} {}".format(root, file_current))
+                
+    print(x)
+#problems in countred
 #everything is empty? RESOLVED: didnt join the file name and the path to that file
+#couldn't find the password in hex? RESOLVED: you must first convert the text file to plain text using a hex converter before reading it.
+#password is 7e5df39cdf57ffcf97fad07c6e7c9d61
